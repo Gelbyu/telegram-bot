883d9d8e334574ce4b95a4d16f60b10ce2f9535c
Kuanchaliev Rustam <r.kuanchaliev@visionlabs.ru>
2023-03-07 18:37:51 +0300

    first commit with bot

---
+++ src/bot.py
@@ -0,0 +1,160 @@
+import logging
+
+import telegram.constants as constants
+from telegram import Update, InlineQueryResultArticle, InputTextMessageContent
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters, InlineQueryHandler
+
+from chatgpt import ChatGPT
+
+class TelegramBot:
+
+    def __init__(self, config: dict, openai: ChatGPT):
+        """
+        Initializes the bot with the given configuration and GPT-3 bot object.
+        :param config: A dictionary containing the bot configuration
+        :param openai: OpenAIHelper object
+        """
+        self.config = config
+        self.openai = openai
+        self.disallowed_message = "Sorry, you are not allowed to use this bot. You can check out the source code at " \
+                                  "https://github.com/n3d1117/chatgpt-telegram-bot"
+
+    async def help(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("/reset - Reset conversation\n"
+                                        "/help - Help menu\n\n",
+                                        disable_web_page_preview=True)
+
+    async def reset(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if not await self.is_allowed(update):
+            logging.warning(f'User {update.message.from_user.name} is not allowed to reset the conversation')
+            await self.send_disallowed_message(update, context)
+            return
+
+        logging.info(f'Resetting the conversation for user {update.message.from_user.name}...')
+
+        chat_id = update.effective_chat.id
+        self.openai.reset_chat_history(chat_id=chat_id)
+        await context.bot.send_message(chat_id=chat_id, text='Done!')
+
+    async def prompt(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """
+        React to incoming messages and respond accordingly.
+        """
+        if not await self.is_allowed(update):
+            logging.warning(f'User {update.message.from_user.name} is not allowed to use the bot')
+            await self.send_disallowed_message(update, context)
+            return
+
+        logging.info(f'New message received from user {update.message.from_user.name}')
+        chat_id = update.effective_chat.id
+
+        await context.bot.send_chat_action(chat_id=chat_id, action=constants.ChatAction.TYPING)
+        response = self.openai.get_chat_response(chat_id=chat_id, query=update.message.text)
+        await context.bot.send_message(
+            chat_id=chat_id,
+            reply_to_message_id=update.message.message_id,
+            text=response,
+            parse_mode=constants.ParseMode.MARKDOWN
+        )
+
+    async def inline_query(self, update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        Handle the inline query. This is run when you type: @botusername <query>
+        """
+        query = update.inline_query.query
+
+        if query == "":
+            return
+
+        results = [
+            InlineQueryResultArticle(
+                id=query,
+                title="Ask ChatGPT",
+                input_message_content=InputTextMessageContent(query),
+                description=query,
+                thumb_url='https://user-images.githubusercontent.com/11541888/223106202-7576ff11-2c8e-408d-94ea-b02a7a32149a.png'
+            )
+        ]
+
+        await update.inline_query.answer(results)
+
+    async def send_disallowed_message(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        """
+        Sends the disallowed message to the user.
+        """
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=self.disallowed_message,
+            disable_web_page_preview=True
+        )
+
+    async def error_handler(self, update: object, context: ContextTypes.DEFAULT_TYPE) -> None:
+        """
+        Handles errors in the telegram-python-bot library.
+        """
+        logging.debug(f'Exception while handling an update: {context.error}')
+
+    def is_group_chat(self, update: Update) -> bool:
+        """
+        Checks if the message was sent from a group chat
+        """
+        return update.effective_chat.type in [
+            constants.ChatType.GROUP,
+            constants.ChatType.SUPERGROUP
+        ]
+
+    async def is_user_in_group(self, update: Update, user_id: int) -> bool:
+        """
+        Checks if user_id is a member of the group
+        """
+        member = await update.effective_chat.get_member(user_id)
+        return member.status in [
+            constants.ChatMemberStatus.OWNER,
+            constants.ChatMemberStatus.ADMINISTRATOR,
+            constants.ChatMemberStatus.MEMBER
+        ]
+
+    async def is_allowed(self, update: Update) -> bool:
+        """
+        Checks if the user is allowed to use the bot.
+        """
+        if self.config['allowed_user_ids'] == '*':
+            return True
+
+        allowed_user_ids = self.config['allowed_user_ids'].split(',')
+
+        # Check if user is allowed
+        if str(update.message.from_user.id) in allowed_user_ids:
+            return True
+
+        # Check if it's a group a chat with at least one authorized member
+        if self.is_group_chat(update):
+            for user in allowed_user_ids:
+                if await self.is_user_in_group(update, user):
+                    logging.info(f'{user} is a member. Allowing group chat message...')
+                    return True
+            logging.info(f'Group chat messages from user {update.message.from_user.name} are not allowed')
+
+        return False
+
+    def run(self):
+        """
+        Runs the bot indefinitely until the user presses Ctrl+C
+        """
+        application = ApplicationBuilder() \
+            .token(self.config['token']) \
+            .proxy_url(self.config['proxy']) \
+            .get_updates_proxy_url(self.config['proxy']) \
+            .build()
+
+        application.add_handler(CommandHandler('reset', self.reset))
+        application.add_handler(CommandHandler('help', self.help))
+        application.add_handler(CommandHandler('start', self.help))
+        application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), self.prompt))
+        application.add_handler(InlineQueryHandler(self.inline_query, chat_types=[
+            constants.ChatType.GROUP, constants.ChatType.SUPERGROUP
+        ]))
+
+        application.add_error_handler(self.error_handler)
+
+        application.run_polling()
